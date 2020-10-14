90ed55710d4fca4df103d640783a48d9a607d13f
Kirill Komarovich <still2405@gmail.com>
2020-10-14 17:32:08 +0300

    Add poll task

---
+++ lib/mix/tasks/poll.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Poll do
+  @moduledoc """
+    Mix task for starting polling updates
+  """
+  use Mix.Task
+  alias Mix.Tasks.Run
+
+  def run(args) do
+    Application.put_env(:marvin, :polling, true, persistent: true)
+    Run.run run_args() ++ args
+  end
+
+  defp run_args do
+    if iex_running?(), do: [], else: ["--no-halt"]
+  end
+
+  defp iex_running? do
+    Code.ensure_loaded?(IEx) and IEx.started?()
+  end
+end
